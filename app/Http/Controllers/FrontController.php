00a395bbc23b7dc0e4fe0cd3360b93b54decc362
mushabberul <dev.sabbirmia@gmail.com>
2022-08-10 19:56:53 +0600

    Get users data via model (ORM,Query Builder)

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class FrontController extends Controller
+{
+    public function UserAllData()
+    {
+       $users = User::all();
+
+       return view('welcome',['users' => $users]);
+
+    }
+}
